0dad424d449968944c0260592224504d2619248c
Morgan Collino <mcollino@stashinvest.com>
2020-04-01 16:58:59 -0400

    Init

---
+++ Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Morgan Collino on 3/31/20.
+//  Copyright © 2020 Morgan Collino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
